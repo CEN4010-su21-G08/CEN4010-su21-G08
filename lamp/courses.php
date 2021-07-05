bfec1a9a98c9940cb3076bb5d2eda22d9b5338d7
Nikita Ostro <nostro2018@fau.edu>
2021-07-05 00:32:13 -0400

    bruh

--- lamp/courses.php
+++ lamp/courses.php
@@ -2,6 +2,8 @@
     $page_title = "Courses";
 ?>
 <?php include('common/header.php'); ?>
+<div class="container">
 <?php include('common/sidebar.php'); ?>
+</div>
 Courses
 <?php include('common/footer.php'); ?>
