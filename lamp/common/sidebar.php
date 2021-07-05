7c46b34922e02dd2553136c5b022f2b1696b9a4f
Nikita Ostro <nostro2018@fau.edu>
2021-07-04 23:23:37 -0400

    Fixing merge issues

---
+++ lamp/common/sidebar.php
@@ -0,0 +1,13 @@
+<aside class="sidebar">
+    <div class="container-fluid sidediv">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Change course
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">CEN4010</a></li>
+                <li><a class="dropdown-item" href="#">ENC1101</a></li>
+            </ul>
+        </div>
+    </div>
+</aside>
---
+++ lamp/courses.php
@@ -0,0 +1,7 @@
+<?php
+    $page_title = "Courses";
+?>
+<?php include('common/header.php'); ?>
+<?php include('common/sidebar.php'); ?>
+Courses
+<?php include('common/footer.php'); ?>
