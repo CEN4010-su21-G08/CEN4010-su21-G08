446861738769db63ac1a05e27263d8e6d278385a
Benjamin Thaw <44248192+bthaw2018@users.noreply.github.com>
2021-07-06 21:00:29 -0400

    Moved input parsing functions to the functions file

--- lamp/lib/functions.php
+++ lamp/lib/functions.php
@@ -26,3 +26,35 @@ function get_channel_type($channel_id) {
     $channel = $result->fetch_assoc();
     return $channel['type'];
 }
+
+function parse_input($field_name, $required=false) {
+    if (!isset($_POST[$field_name])) {
+        if ($required) throwError(400, "Invalid input. Missing field " . $field_name);
+        else return NULL;
+    }
+    $data = $_POST[$field_name];
+    if (empty($data)) {
+        if ($required) throwError(400, "Invalid input. Empty field " . $field_name);
+        else return NULL;
+    }
+    $data = trim($data);
+    $data = stripslashes($data);
+    // $data = htmlspecialchars($data);
+    return $data;
+}
+
+function parse_get_input($field_name, $required=false) {
+    if (!isset($_GET[$field_name])) {
+        if ($required) throwError(400, "Invalid input. Missing field " . $field_name);
+        else return NULL;
+    }
+    $data = $_GET[$field_name];
+    if (empty($data)) {
+        if ($required) throwError(400, "Invalid input. Empty field " . $field_name);
+        else return NULL;
+    }
+    $data = trim($data);
+    $data = stripslashes($data);
+    // $data = htmlspecialchars($data);
+    return $data;
+}
--- lamp/lib/page-setup.php
+++ lamp/lib/page-setup.php
@@ -20,38 +20,5 @@
         }
     }
 
-
-    function parse_input($field_name, $required=false) {
-        if (!isset($_POST[$field_name])) {
-            if ($required) throwError(400, "Invalid input. Missing field " . $field_name);
-            else return NULL;
-        }
-        $data = $_POST[$field_name];
-        if (empty($data)) {
-            if ($required) throwError(400, "Invalid input. Empty field " . $field_name);
-            else return NULL;
-        }
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-    function parse_get_input($field_name, $required=false) {
-        if (!isset($_GET[$field_name])) {
-            if ($required) throwError(400, "Invalid input. Missing field " . $field_name);
-            else return NULL;
-        }
-        $data = $_GET[$field_name];
-        if (empty($data)) {
-            if ($required) throwError(400, "Invalid input. Empty field " . $field_name);
-            else return NULL;
-        }
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
     
 
