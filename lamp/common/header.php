e6db92faf5730e1b6e23bf3705d8de16fdeaa4eb
Benjamin Thaw <44248192+bthaw2018@users.noreply.github.com>
2021-07-28 20:05:54 -0400

    Started working on a new member modal - added bootstrap icons - there's some test code for working modals, will finish in a later commit - added an empty manage course page (will contain course & group management options)

--- lamp/common/header.php
+++ lamp/common/header.php
@@ -11,6 +11,8 @@ global $center_page;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo(isset($page_title) ? $page_title . " - " : ""); ?>Burrow</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="./static/main.css" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   </head>
