85d4042843bf91f46b23966ac59f28e70e9f0f66
Benjamin Thaw <44248192+bthaw2018@users.noreply.github.com>
2021-07-30 17:09:14 -0400

    Started reporting feature CSG-52 - Added reported classes (not finished) CSG-51 - Worked with Nik, Garrett, Ray, and Christian
    Co-authored-by: Nikita Ostro <85267383+Nostro2018@users.noreply.github.com>
    Co-authored-by: Raymond Budoff-Kingsland <rbudoffkings2018@fau.edu>
    Co-authored-by: Garrett Reardon <greardon2018@fau.edu>
    Co-authored-by: Christian Engman <cengman2017@fau.edu>

---
+++ lamp/lib/report-handler.php
@@ -0,0 +1,110 @@
+<?php
+
+class Report
+{
+    public $r_id = null;
+    public $reported = null;
+    public $reporter = null;
+    public $report_date = null;
+    public $reason = null;
+    public $m_id = null;
+    public $ch_id = null;
+    public $course_id = null;
+    public $message = null;
+
+    function __construct($r_id = null, $reported = null, $reporter = null, $report_date = null, $reason = null, $m_id = null, $ch_id = null, $course_id = null, $message = null) {
+        if ($r_id == null) {
+            // do nothing
+        } else if ($reported == null) {
+            // query db with $r_id
+            global $conn;
+            
+            $sql = "SELECT * FROM `reports` WHERE `r_id` = '" . $conn->real_escape_string($r_id) .  "'";
+            $result = $conn->query($sql);
+
+            $numRows = mysqli_num_rows($result);
+            if ($numRows <= 0) {
+                // do nothing (no matching report)
+            } else {
+                $report = $result->fetch_assoc();
+                
+                $this->r_id = $report['r_id'];
+                $this->reported = $report['reported'];
+                $this->reporter = $report['reporter'];
+                $this->report_date = $report['report_date'];
+                $this->reason = $report['reason'];
+                $this->m_id = $report['m_id'];
+                $this->ch_id = $report['ch_id'];
+                $this->course_id = $report['course_id'];
+                $this->message = $report['message'];
+            }
+
+        } else {
+            $this->r_id = $r_id;
+            $this->reported = $reported;
+            $this->reporter = $reporter;
+            $this->report_date = $report_date;
+            $this->reason = $reason;
+            $this->m_id = $m_id;
+            $this->ch_id = $ch_id;
+            $this->course_id = $course_id;
+            $this->message = $message;
+        }
+    }
+
+    public static function get($r_id) {
+        // helper function
+        return new Report($r_id);
+    }
+
+    public static function list_courseReports($course_id) {
+
+    }
+    
+    public static function list_reportedUser($user_id) {
+        
+    }
+
+    public static function list_reporter($user_id) {
+    }
+
+    public static function create($reported, $reason, $m_id, $ch_id) {
+        global $user;
+        $reporter = $user->uid;
+
+        $channel = new Channel($ch_id);
+        $course_id = $channel->course_id;
+
+        $m = Message::get($m_id);
+        $message = $message->message;
+    }
+
+    
+
+
+    public function delete() {
+
+    }
+
+    public function ignore() {
+
+    }
+
+    /* Report Actions */
+    public function deleteMessage() {
+
+    }
+
+    public function muteUser() {
+
+    }
+
+    public function kickUser() {
+
+    }
+
+    public function banUser() {
+
+    }
+}
+
