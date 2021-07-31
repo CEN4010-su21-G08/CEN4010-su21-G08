818e56dad2d157d8805be1de6bdbc190a7dfe1aa
Benjamin Thaw <44248192+bthaw2018@users.noreply.github.com>
2021-07-31 17:32:03 -0400

    Moved Bootstrap JS to header

--- lamp/common/header.php
+++ lamp/common/header.php
@@ -15,6 +15,7 @@ global $center_page;
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="./static/main.css" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </head>
   <body>
   <header>
