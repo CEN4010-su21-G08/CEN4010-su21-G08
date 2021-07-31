2940cb8cc60793c70d0ac5c7aa45ee6d1bb50cad
Benjamin Thaw <44248192+bthaw2018@users.noreply.github.com>
2021-07-30 23:04:40 -0400

    removed testing code from reports.php

--- lamp/reports.php
+++ lamp/reports.php
@@ -6,15 +6,10 @@ $center_page = true;
 <?php require_once("lib/report-handler.php"); ?>
 <?php include('common/header.php'); ?>
 <?php
-$report = new Report("123");
-$report->delete();
-Report::list_courseReports("abc");
+
 ?>
 <div>
-    <?php /* Just demoing things */ ?>
-    <p>Report ID: <code><?= htmlspecialchars($report->r_id); ?></code></p>
-    <p>Reason: <code><?= htmlspecialchars($report->reason); ?></code></p>
-    <!-- <p>Reason URL: <?= urlencode($report->reason); ?></p> -->
+    
 </div>
 
 <?php include('common/footer.php'); ?>
