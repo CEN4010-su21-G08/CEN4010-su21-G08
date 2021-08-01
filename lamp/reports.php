4942060c5620d563f15f1852142eea25890a85cf
Benjamin Thaw <44248192+bthaw2018@users.noreply.github.com>
2021-07-31 20:56:05 -0400

    Started reports page (added create ability) - Created logical structure of reports page - Implemented POST create endpoint to create a report

--- lamp/reports.php
+++ lamp/reports.php
@@ -4,12 +4,108 @@ $center_page = true;
 ?>
 <?php require_once("lib/page-setup.php") ?>
 <?php require_once("lib/report-handler.php"); ?>
-<?php include('common/header.php'); ?>
+<?php require_once("lib/course-handler.php"); ?>
+<?php require_once("lib/channel-handler.php"); ?>
 <?php
+/**
+ * POST
+ *  ?action=
+ *      create: create a report on a message
+ *      act: perform an action on a report (delete message, ignore, etc)
+ *      
+ * GET
+ *  ?action=
+ *      list (or blank): list reports, possibly within a course
+ *      get: get details about a single report
+ */
 
 ?>
-<div>
-    
-</div>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    header("Content-Type: application/json");
+    $response = [];
+    function sendResponse()
+    {
+        global $response;
+        echo json_encode($response);
+        die();
+        return;
+    }
+    function sendError($error, $status = 500)
+    {
+        global $response;
+        $response = [];
+        $response['error'] = $error;
+        $response['status'] = $status;
+        http_response_code($status);
+        sendResponse();
+    }
 
-<?php include('common/footer.php'); ?>
+
+    if (isset($_GET['action'])) {
+        $action = $_GET['action'];
+        if ($action == "create") {
+            // create a report from the user
+
+            // first make sure the request body is valid
+            // we need a message ID, channel ID, and a reason
+            if (!isset($_POST['m_id']) || empty($_POST['m_id'])) {
+                sendError("No message reported", 400);
+            }
+            if (!isset($_POST['ch_id']) || empty($_POST['ch_id'])) {
+                sendError("Mismatched channel provided", 400);
+            }
+            if (!isset($_POST['reason']) || empty($_POST['reason'])) {
+                sendError("No report reason provided", 400);
+            }
+            $m_id = $_POST['m_id'];
+            $ch_id = $_POST['ch_id'];
+            $reason = $_POST['reason'];
+
+            $channel = new Channel($ch_id);
+            if ($channel->get_role() == 0) {
+                sendError("Forbidden", 403);
+            }
+            $message = Message::get($m_id);
+            if ($message->ch_id == null || $message->ch_id !== $ch_id) {
+                sendError("Mismatched channel provided", 400);
+            }
+
+            $create_response = Report::create($reason, $message->m_id, $channel->ch_id);
+            if (isset($create_response['error'])) {
+                if (isset($create_response['status'])) {
+                    http_response_code($create_response['status']);
+                }
+                sendError($create_response['error'], $create_response['status']);
+            }
+            $response['success'] = true;
+            sendResponse();
+        } else if ($action == "act") {
+        } else {
+            sendError("Invalid action provided", 400);
+        }
+    } else {
+        sendError("No action provided", 400);
+    }
+} else if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    include('common/header.php');
+    if (!isset($_GET['action']) || $_GET['action'] == 'list') {
+        // List reports
+
+    } else {
+        $action = $_GET['action'];
+        if ($action == "get") {
+        } else { ?>
+            <div class="alert alert-danger">
+                Invalid action
+            </div>
+<?php }
+    }
+    include('common/footer.php');
+} else {
+    http_response_code(400);
+    echo "Invalid response";
+}
+?>
+
+<?php ?>
