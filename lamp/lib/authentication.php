2df84c3467483a6041ff934ebded9c198d333164
Garrett <greardon2018@fau.edu>
2021-08-03 17:28:13 -0400

    added change password method, change first name method, change last name method

--- lamp/lib/authentication.php
+++ lamp/lib/authentication.php
@@ -380,6 +380,7 @@
             }
             $conn->query($sql);
         }
+        
         private static function validate_password($password)
         {
             $one_upper = preg_match("/[A-Z]/", $password);
@@ -395,6 +396,59 @@
 
             if (strlen($password) >= 8 && $cnt >= 3) return true;
             else return false;
+        }
+
+        public function change_password($old, $new, $new_conf) {
+            global $conn;
+
+            if (!isset($new) || $new == null || !is_string($new) || empty($new)) {
+                return "New password is required.";
+            }
+            if (!isset($old) || $old == null || !is_string($old) || empty($old)) {
+                return "Old password is required.";
+            }
+            if (password_verify($old, $this->password)) {
+                return "Old password is incorrect.";
+            }
+            if ($old == $new) {
+                return "New password must be different than old password.";
+            }
+            if ($new == $new_conf) {
+                return "Verified Password does not match.";
+            }
+
+            if (!User::validate_password($new)) {
+                return "Please enter a valid password.";
+            }else {
+                $sql = "UPDATE `users` SET `password` = '" . $conn->real_escape_string($new) . "' WHERE `uid` = '" . $conn->real_escape_string($this->uid) . "'";
+            }
+            $conn->query($sql);
+        }
+
+        public function change_first($new_first) {
+            global $conn;
+
+            if (!isset($new_first) || $new_first = null || !is_string($new_first) || empty($new_first)) {
+                return "Please enter a first name";
+            }
+            if ($new_first != $this->first_name) {
+                $sql = "UPDATE `users` SET `first_name` = '" . $conn->real_escape_string($new_first) . "' WHERE `uid` = '" . $conn->real_escape_string($this->uid) . "'";
+            }
+
+            $conn->query($sql);
+        }
+
+        public function change_last($new_last) {
+            global $conn;
+
+            if (!isset($new_last) || $new_last = null || !is_string($new_last) || empty($new_last)) {
+                return "Please enter a last name";
+            }
+            if ($new_last != $this->last_name) {
+                $sql = "UPDATE `users` SET `last_name` = '" . $conn->real_escape_string($new_last) . "' WHERE `uid` = '" . $conn->real_escape_string($this->uid) . "'";
+            }
+
+            $conn->query($sql);
         }
     }
 
