bf64cbd8add3aaff539c7e78f7ead32ec0375043
Nikita Ostro <nostro2018@fau.edu>
2021-08-03 23:42:14 -0400

    Added UI for "Manage All Courses" page

--- lamp/manage-all-courses.php
+++ lamp/manage-all-courses.php
@@ -1,34 +1,35 @@
 <?php
-    $page_title = "Manage Courses (ADMIN)";
-    $center_page = true;
+$page_title = "Manage Courses (ADMIN)";
+$center_page = true;
 ?>
 <?php
-    require_once("lib/page-setup.php");
-    require_once("lib/course-handler.php");
-    require_once("lib/authentication.php");
+require_once("lib/page-setup.php");
+require_once("lib/course-handler.php");
+require_once("lib/authentication.php");
 ?>
 <?php
-    include('./common/header.php');
-    if (!($user->is_admin())) { ?>
-        <div class="alert alert-danger" style="margin: 20px;">You don't have access to this page or it doesn't exist</div>
-        <?php }
-    else
-    { ?>
-        <?php
-        $courses = Course::get_all_courses();
-        ?>
-        <a href="create-course.php">Create a Course</a>
-        <?php
-        foreach ($courses as $course)
-        { ?>
-            <br/>
-            <br/>
-            <h1><?= $course->course_code ?></h1>
-            <?php if (isset($course->course_name)) { ?>
-                <h3><?= $course->course_name ?></h3>
-            <?php } ?> 
-            <a href="manage-course.php?course_id=<?=urlencode($course->course_id)?>">Manage Course</a><br/>
-            <a href="delete-course.php?course_id=<?=urlencode($course->course_id)?>">Delete Course</a><br/>
+include('./common/header.php');
+if (!($user->is_admin())) { ?>
+    <div class="alert alert-danger" style="margin: 20px;">You don't have access to this page or it doesn't exist</div>
+<?php } else { ?>
+    <h2 class="maintitleheader">All Courses</h2>
+    <hr />
+    <br />
+    <div class="d-grid col-6 mx-auto">
+        <a href="create-course.php" class="btn btn-outline-primary">Create Course</a>
+    </div>
+
+    <?php
+    $courses = Course::get_all_courses();
+    ?>
+    <br />
+    <br />
+    <ul class="list-group all-course-list">
+        <?php foreach ($courses as $course) { ?>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <a><?= htmlspecialchars($course->course_code . '-' . $course->section_number); ?></a>
+                <span><a href="manage-course.php?course_id=<?= urlencode(htmlspecialchars($course->course_id)); ?>" class="btn btn-outline-dark">Manage</a></span>
+            </li>
         <?php } ?>
-    <?php }
-        
+    </ul>
+<?php }
